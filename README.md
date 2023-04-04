8487be4582472721ca32575e9ab729e9fbd4a525
Elias Rohrer <dev@tnull.de>
2023-04-04 12:50:10 +0200

    Add initial project structure

---
+++ README.md
@@ -0,0 +1,3 @@
+# ldk-lsp-client
+
+Types and primitives to integrate a [spec-compliant](https://github.com/BitcoinAndLightningLayerSpecs/lsp) LSP with an LDK-based node.
---
+++ src/lib.rs
@@ -0,0 +1,22 @@
+// This file is Copyright its original authors, visible in version contror
+// history.
+//
+// This file is licensed under the Apache License, Version 2.0 <LICENSE-APACHE
+// or http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your option.
+// You may not use this file except in accordance with one or both of these
+// licenses.
+#![crate_name = "ldk_lsp_client"]
+
+//! # ldk-lsp-client
+//! Types and primitives to integrate a spec-compliant LSP with an LDK-based node.
+#![deny(missing_docs)]
+#![deny(broken_intra_doc_links)]
+#![deny(private_intra_doc_links)]
+#![allow(bare_trait_objects)]
+#![allow(ellipsis_inclusive_range_patterns)]
+#![cfg_attr(docsrs, feature(doc_auto_cfg))]
+
+mod channel_request;
+mod jit_channel;
+mod transport;
